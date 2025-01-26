3310982751dbb2886b422750d82fe2911fc01bcc
LayBalt <somi.kyy@mail.ru>
2025-01-27 00:00:36 +0100

    first commit

---
+++ src/main/java/org/snetwork/snfishing/SNFishing.java
@@ -0,0 +1,17 @@
+package org.snetwork.snfishing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SNFishing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
